c562bce0212c273613414a26620a443655a8c8eb
prod_upal <tim.ism06@gmail.com>
2024-11-13 23:01:48 +0500

    Добавьте файлы проекта.

---
+++ AddEditPage.xaml.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KPIsmagilov
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private BitmapImage imageSource;
+
+        private Personnel currentPersonnel = new Personnel();
+        public AddEditPage(Personnel personnel)
+        { 
+            InitializeComponent();
+
+            var UnitsTypes = KPIsmagilovEntities.GetContext().Units.Select(p => p.ID_Units).ToList();
+
+            foreach (var UnitsType in UnitsTypes)
+            {
+                UnitsTypeComboBox.Items.Add(UnitsType);
+            }
+
+            if(personnel == null) {
+                DeleteButton.Visibility = Visibility.Collapsed;
+                SaveButton.Margin = new Thickness(0);
+                imageSource = new BitmapImage(new Uri("C:\\Users\\prod_upal\\Desktop\\KP\\KPIsmagilov\\Resources\\default_photo.png"));
+            }
+            else
+            {
+                currentPersonnel = personnel;
+                if(personnel.Photo != null) 
+                    imageSource = new BitmapImage(new Uri(personnel.Photo));
+                else
+                {
+                    MessageBox.Show("не удалось загрузить фотографию, либо она отсутствует");
+                    imageSource = new BitmapImage(new Uri("C:\\Users\\prod_upal\\Desktop\\KP\\KPIsmagilov\\Resources\\default_photo.png"));
+                }
+            }
+            UserImage.Source = imageSource;
+            DataContext = currentPersonnel;
+        }
+
+        private void ChoisePhoto_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string selectedFilePath = openFileDialog.FileName;
+
+                    BitmapImage image = new BitmapImage(new Uri(selectedFilePath));
+                    UserImage.Source = image;
+                    PhotoTBox.Text = selectedFilePath;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при добавлении фото: {ex.Message}", "Ошибка");
+                }
+
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime date2 = (DateTime)YearOfBirthDPicker.SelectedDate;
+            DateTime date1 = (DateTime)YearOfEntryIntoServiceDPicker.SelectedDate;
+            int res = (int)date1.Year - date2.Year;
+                        
+            StringBuilder errors = new StringBuilder();
+            if (currentPersonnel.ID_Units == null)
+                errors.AppendLine("Укажите ID части");
+            if (string.IsNullOrWhiteSpace(currentPersonnel.Surname))
+                errors.AppendLine("Укажите фамилию военнослужащего");
+            if (string.IsNullOrWhiteSpace(currentPersonnel.Name))
+                errors.AppendLine("Укажите имя военнослужащего");
+            if (string.IsNullOrWhiteSpace(currentPersonnel.Patronymic))
+                errors.AppendLine("Укажите отчество военнослужащего");
+            if (string.IsNullOrWhiteSpace(currentPersonnel.Position))
+                errors.AppendLine("Укажите звание военнослужащего");
+            if (YearOfBirthDPicker.SelectedDate == new DateTime(1,1,1))
+                errors.AppendLine("Укажите год рождения военнослужащего");
+            if (YearOfEntryIntoServiceDPicker.SelectedDate == new DateTime(1, 1, 1))
+                errors.AppendLine("Укажите год вступления в службу военнослужащего");
+            if (currentPersonnel.LengthOfService == 0)
+                errors.AppendLine("Укажите выслугу лет военнослужащего");
+            if (YearOfBirthDPicker.SelectedDate > YearOfEntryIntoServiceDPicker.SelectedDate && 
+                !(YearOfBirthDPicker.SelectedDate == new DateTime(1, 1, 1) && YearOfEntryIntoServiceDPicker.SelectedDate == new DateTime(1, 1, 1))) 
+                errors.AppendLine("Год рождения военнослужащего не может быть позднее года вступления в службу");
+            else if (res < 18 &&
+                !(YearOfBirthDPicker.SelectedDate == new DateTime(1, 1, 1) && YearOfEntryIntoServiceDPicker.SelectedDate == new DateTime(1, 1, 1)))
+                errors.AppendLine("Вступление на службу моложе 18 лет невозможно");
+
+            currentPersonnel.YearOfBirth = date2;
+            currentPersonnel.YearOfEntryIntoService = date1;
+            currentPersonnel.Photo = PhotoTBox.Text;
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (currentPersonnel.ID_serviceman == 0)
+            {
+                KPIsmagilovEntities.GetContext().Personnel.Add(currentPersonnel);
+            }
+            try
+            {
+                KPIsmagilovEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void YearOfEntryIntoServiceDPicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime date = (DateTime)YearOfEntryIntoServiceDPicker.SelectedDate;
+            int res = (int)date.Year;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentPersonnel = (sender as Button).DataContext as Personnel;
+
+            if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    KPIsmagilovEntities.GetContext().Personnel.Remove(currentPersonnel);
+                    KPIsmagilovEntities.GetContext().SaveChanges();
+                    Manager.MainFrame.GoBack();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            
+        }
+    }
+}
---
+++ PersonnelPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KPIsmagilov
+{
+    /// <summary>
+    /// Логика взаимодействия для PersonnelPage.xaml
+    /// </summary>
+    public partial class PersonnelPage : Page
+    {
+        public PersonnelPage()
+        {
+            InitializeComponent();
+
+            var currentPersonnel = KPIsmagilovEntities.GetContext().Personnel.ToList();
+            PersonnelListView.ItemsSource = currentPersonnel;
+
+            ProdAll.Text = Convert.ToString(currentPersonnel.Count);
+            ProdAtTheMoment.Text = Convert.ToString(currentPersonnel.Count);
+        }
+
+        private void UpdatePersonnel()
+        {
+            var currentPersonnel = KPIsmagilovEntities.GetContext().Personnel.ToList();
+
+            currentPersonnel = currentPersonnel.Where(p => p.Surname.ToLower().Contains(TBSearch.Text.ToLower()) || 
+                                                        p.Name.ToLower().Contains(TBSearch.Text.ToLower()) || 
+                                                        p.Patronymic.ToLower().Contains(TBSearch.Text.ToLower()) || 
+                                                        p.Position.ToLower().Contains(TBSearch.Text.ToLower()) || 
+                                                        (p.LengthOfService).ToString().ToLower().Contains(TBSearch.Text.ToLower()) || 
+                                                        (p.YearOfBirth).ToString().ToLower().Contains(TBSearch.Text.ToLower())||
+                                                        (p.YearOfEntryIntoService).ToString().ToLower().Contains(TBSearch.Text.ToLower())).ToList();
+
+            if (SortCombo.SelectedIndex == 0)
+            {
+                currentPersonnel = currentPersonnel.OrderBy(p => p.LengthOfService).ToList();
+            }
+
+            if (SortCombo.SelectedIndex == 1)
+            {
+                currentPersonnel = currentPersonnel.OrderByDescending(p => p.LengthOfService).ToList();
+            }
+
+
+            ProdAtTheMoment.Text = Convert.ToString(currentPersonnel.Count);
+
+            if (CostUp.IsChecked.Value)
+            {
+                currentPersonnel = currentPersonnel.Where(p => p.Awards != null).ToList();
+                ProdAtTheMoment.Text = Convert.ToString(currentPersonnel.Count);
+            }
+            if (CostDown.IsChecked.Value)
+            {
+                currentPersonnel = currentPersonnel.Where(p => p.Awards == null).ToList();
+                ProdAtTheMoment.Text = Convert.ToString(currentPersonnel.Count);
+            }
+
+            PersonnelListView.ItemsSource = currentPersonnel;
+        }
+
+        private void TBSearch_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UpdatePersonnel();
+        }
+
+        private void CostUp_Checked(object sender, RoutedEventArgs e)
+        {
+                UpdatePersonnel();
+        }
+
+        private void CostDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdatePersonnel();
+        }
+
+        private void SortCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdatePersonnel();
+        }
+
+        private void EditContextButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Personnel));
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var currentPersonnel = KPIsmagilovEntities.GetContext().Personnel.ToList();
+
+            PersonnelListView.ItemsSource = currentPersonnel;
+        }
+
+        private void ResetFilter_Click(object sender, RoutedEventArgs e)
+        {
+            CostUp.IsChecked = false;
+            CostDown.IsChecked = false;
+            UpdatePersonnel();
+        }
+    }
+}
